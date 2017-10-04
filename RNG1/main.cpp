e550206d01474117a55d3c12f73401c2be3e4edf
RazoHound <p15234851.my365.dmu.ac.uk>
2017-10-04 12:09:02 +0100

    First commit of the project

---
+++ RNG1/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+int main() {
+	int inGuesses;
+	int icMin;
+	int icMax;
+	int iguess;
+	inGuesses = 5;
+	icMin = 1;
+	icMax = 100;
+	int icNumber;
+	srand(time(nullptr));
+	icNumber = rand() % icMax + icMin;
+	
+	cout << " The computer has thought of a number between " << icMin << " and " << icMax << " you have " << inGuesses << " guesses Good Luck!!" << endl;
+
+	bool bnGuessed = false;
+
+
+	while (bnGuessed == false&& inGuesses != 0) {
+	cout<<"Please input a number" << endl;
+	cin >> iguess;
+
+
+	if (iguess<icMin || iguess>icMax ) {
+		cout << "Please input a valid number" << endl;
+		cout << iguess << endl;
+	}
+
+	else if (iguess == icNumber) {
+		inGuesses = inGuesses - 1;
+		cout << "Good job you guessed with " << inGuesses << " guesses remaining" << endl;
+		bnGuessed = true;
+	}
+
+
+
+	else if (iguess < icNumber) {
+		inGuesses = inGuesses - 1;
+		if (inGuesses != 0) {
+			cout << "Looks like you need to guess Higher " << inGuesses << " guesses remaining" << endl;
+		}
+		else {
+			cout << "You did not get it, it was" << icNumber << " better luck next time!!!" << endl;
+		}
+	}
+
+
+	else if (iguess > icNumber) {
+		inGuesses = inGuesses - 1;
+		if (inGuesses != 0) {
+			cout << "Looks like you need to guess Lower " << inGuesses << " guesses remaining" << endl;
+		}
+		else {
+			cout << "You did not get it, it was "<< icNumber<<" better luck next time!!!"<< endl;
+		}
+	}
+
+	}
+
+
+
+	system("pause");
+	return 0;
+}
